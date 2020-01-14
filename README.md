c05bc393c4136e1a604b8fa46a7ab58dc084da24
benjamin thom and jozy kinnaman <jozy@>
2020-01-14 11:48:16 -0800

    create two images and update index.HTML, styles.css, and scripts.js to show and hide the two additional walrus images, stylize the show images/hide images buttons, and change the cursor when hovering over images buttons text.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Peekaboo
+
+#### A simple webpage demonstrating the use of JQuery with CSS.
+
+#### By Ben and Jozy Kinnaman
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $("walrus-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#walrus-showing") .slideToggle();
+    $("#walrus-hidden").fadeToggle();
+  });
+  $(".clickable1").click(function() {
+    $("#walrus-show") .toggle();
+    $("#walrus-hiding").toggle();
   });
 });
